114a60503d540a68d4e1e2de8380e758250b0ec2
MacBoq <matissegouery@yahoo.fr>
2024-11-01 20:27:28 +0100

    fix css

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-undef */
-const express = require('express')
-const path = require('path')
-const app = express()
-const port = 3000
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
 
-app.listen(port,() =>{
-    console.log(`Server is up http://192.168.1.48:${port} or localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server is up http://192.168.1.48:${port} or localhost:${port}`);
+});
