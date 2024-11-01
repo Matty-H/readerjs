6f259d0092d2a715910b1c0da6453edfbcc197b6
MacBoq <matissegouery@yahoo.fr>
2024-11-01 19:46:35 +0100

    fix reader and add eslint

--- public/client.js
+++ public/client.js
@@ -3,19 +3,28 @@ let page_displayed = 2
 const MIN_PAGE = 0
 const MAX_PAGE = 7
 
-function create_image() {
+let src = document.getElementsByClassName('reader')[0]
+
+function generate_page() {
     let images = {}
-    let src = document.getElementsByClassName('reader')[0]
     for (let i = 1; i <= page_displayed; i++) {
-        console.log(i)
-        images[`img_${i}`] = document.createElement('img')
-        images[`img_${i}`].src = `F4U-book/F4U-000${counter+i-1}.webp`
-        images[`img_${i}`].className = `reader_img_${i}`
-//        images[`img_${i}`].style.maxHeight = '100vh'
-        src.appendChild(images[`img_${i}`])
+        create_image(images[`img_${i}`], `F4U-book/F4U-000${counter+i-1}.webp`, `reader_img_${i}`, '100vmin', '100vmin')
     }
 }
 
+function create_image(image, source, divname, tall, large) {
+    image = document.createElement('img')
+    image.src = source
+    image.className = divname
+    set_size_image(image, tall, large)
+    src.appendChild(image)
+}
+
+function set_size_image(image, tall, large) {
+    image.style.maxHeight = tall
+    image.style.maxWidth = large
+    image.style.objectFit = 'contain'
+}
 function set_image() {
     let images = {}
     for (let i = 1; i <= page_displayed; i++) {
@@ -29,7 +38,6 @@ function prev_page() {
         counter -= 2
         set_image()
     }
-    console.log(counter)
 }
 
 function next_page() {
@@ -40,17 +48,14 @@ function next_page() {
 }
 
 function page_display() {
-    if (page_displayed == 1) {
-        page_displayed = 2
+    let img = document.getElementsByClassName(`reader_img_2`)[0]
+    
+    if (!img) {
+        create_image(img, `F4U-book/F4U-000${counter+1}.webp`, 'reader_img_2', '100vmin', '100vmin')
     }
     else {
-        page_displayed = 1
-        let img = document.getElementsByClassName(`reader_img_2`)[0]
-        if (img) {
-            img.parentNode.removeChild(img)
+        src.removeChild(img)
     }
 }
-console.log('Nb of pages displayed is now '+page_displayed)
-}
 
-create_image()
+generate_page()
